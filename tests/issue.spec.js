03319750a8b5b82c7668b3653927aa8a6350ce37
alex-vukov <alexvukov@gmail.com>
2023-03-28 09:43:53 +0300

    Adjust example

--- tests/issue.spec.js
+++ tests/issue.spec.js
@@ -3,11 +3,13 @@ import { test, expect } from "@playwright/test";
 test("user is able to login", async ({ page }) => {
   await page.goto("/");
 
+  let postData;
   await page.route("**/login", async (route, request) => {
-    await expect (request.postData()).toBeTruthy();
-    await route.fulfill({json: '{}'});
+    postData = await request.postData();
+    await route.fulfill({ json: "{}" });
   });
 
   const loginButton = page.getByTestId("login-button");
   await Promise.all([loginButton.click(), page.waitForResponse("**/login")]);
+  await expect.poll(() => postData).toBeTruthy();
 });
